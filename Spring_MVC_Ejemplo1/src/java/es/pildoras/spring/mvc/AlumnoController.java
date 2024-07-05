2a0ad8bfa033c90ce1aeb6e3bd4b8672afb76bc6
Julio Martinez <sk8182enkidu@gmail.com>
2024-07-05 09:54:26 +0200

    Validación de Formularios IV @INITBINDER - video 41

--- Spring_MVC_Ejemplo1/src/java/es/pildoras/spring/mvc/AlumnoController.java
+++ Spring_MVC_Ejemplo1/src/java/es/pildoras/spring/mvc/AlumnoController.java
@@ -2,9 +2,12 @@ package es.pildoras.spring.mvc;
 
 
 import javax.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/alumno")
 public class AlumnoController {
+    
+    @InitBinder
+    public void miBinder(WebDataBinder binder){
+        
+        StringTrimmerEditor recortaEspaciosBlanco = new StringTrimmerEditor(true);
+        
+        binder.registerCustomEditor(String.class, recortaEspaciosBlanco);
+        
+    }
+    
 
     @RequestMapping("/muestraFormulario")
     public String muestraFormulario(Model modelo) {
