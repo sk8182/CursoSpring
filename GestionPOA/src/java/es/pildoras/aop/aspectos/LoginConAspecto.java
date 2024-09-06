add44a06d474afea419e4a662401bce71aff76fa
Julio Martinez <sk8182enkidu@gmail.com>
2024-09-06 12:11:32 +0200

    Curso Spring. AOP V. Reutilización de pointcut expressions. Vídeo 80

--- GestionPOA/src/java/es/pildoras/aop/aspectos/LoginConAspecto.java
+++ GestionPOA/src/java/es/pildoras/aop/aspectos/LoginConAspecto.java
@@ -6,6 +6,7 @@ package es.pildoras.aop.aspectos;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,10 +17,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoginConAspecto {
     
+    
+//    @Pointcut("execution(public * insertaCliente*(..))")//para no copiar la ruta del @Before en todas las lineas
+//    private void paraClientes(){
+//        
+//    }
+    
+     @Pointcut("execution(public * insertaCliente(..))")//esta solo en los clientes normales realiza la comprobación.
+    private void paraClientes(){
+        
+    }
+    
     //@Before("execution(public void es.pildoras.aop.dao.ClienteVIPDAO.insertaCliente())")//esto es un ejemplo de PointCut Expression
     //@Before("execution(public * insertaCliente*())")//esto es otro ejemplo de PointCut Expression, donde el asterisco nos filtra los tipos de datos que devuelve o no devuelve (String y void) y lo que viene despues del asterisco tb
     //@Before("execution(public * insertaCliente*(es.pildoras.aop.dao.Cliente,..))")//así para pasar parametros
-    @Before("execution(public * insertaCliente*(..))")//así para pasar todo
+    @Before("paraClientes()")//así para pasar todo
     public void antesInsertarCliente(){
         
         System.out.println("El usuario está logueado");
@@ -28,4 +40,17 @@ public class LoginConAspecto {
         
     }
     
+    @Before("paraClientes()")
+    public void requisitosCliente(){
+        
+        System.out.println("El cliente cumple los requisitos para ser insertado en la BBDD");
+        
+    }
+    
+    @Before("paraClientes()")
+    public void requisitosTabla(){
+        
+        System.out.println("La tabla contiene menos de 3000 registros, puedes insertar el nuevo cliente");
+        
+    }
 }
