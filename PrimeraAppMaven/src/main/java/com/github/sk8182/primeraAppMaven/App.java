126950f2205d66cc0735803c3cd90044d70f646b
Julio Martinez <sk8182enkidu@gmail.com>
2024-09-09 18:44:37 +0200

    Curso Spring. Maven varios

---
+++ PrimeraAppMaven/src/main/java/com/github/sk8182/primeraAppMaven/App.java
@@ -0,0 +1,10 @@
+package com.github.sk8182.primeraAppMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
