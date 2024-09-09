f5a991a23a21c331b5818cb98c8a6010c400b9fd
Julio Martinez <sk8182enkidu@gmail.com>
2024-09-09 19:02:19 +0200

    Curso Spring. Seguridad II. Vídeo 93 dependencias cargadas en POM, inicio de proyecto

---
+++ SeguridadSpring/src/main/java/es/pildoras/seguridadspring/App.java
@@ -0,0 +1,10 @@
+package es.pildoras.seguridadspring;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
